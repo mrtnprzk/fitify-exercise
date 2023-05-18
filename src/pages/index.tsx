a9bd98a02ae7ba65f3a1d13c130e90de1e01d1b7
mrtnprzk <marti.parizek@gmail.com>
2023-05-18 14:50:19 +0200

    [UPDATE] Image next.js warnings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="text-green-500">
-      <Image src="images/fitify.svg" alt="Fitify" width={'300'} height={'150'}/>
+      <Image priority src="images/fitify.svg" alt="Fitify Logo" width={200} height={75} className="h-auto w-40"/>
       Hello Fitify!
     </main>
   );
