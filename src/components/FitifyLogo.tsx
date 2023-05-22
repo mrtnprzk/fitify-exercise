d50f33b953df4befc26c1800aa89745a97881d43
mrtnprzk <marti.parizek@gmail.com>
2023-05-22 21:31:18 +0200

    [UPDATE] Fitify logo linked to github repo

--- src/components/FitifyLogo.tsx
+++ src/components/FitifyLogo.tsx
@@ -1,15 +1,12 @@
 import { FC } from "react";
 import Image from "next/image";
+import Link from "next/link";
+
+const githubRepository = "https://github.com/mrtnprzk/fitify-exercise";
 
 const FitifyLogo: FC = (): JSX.Element => (
-  <Image
-    priority
-    src="images/fitify.svg"
-    alt="Fitify Logo"
-    width={200}
-    height={75}
-    className="h-auto w-60"
-  />
+    <Link href={githubRepository}>
+        <Image priority src="images/fitify.svg" alt="Fitify Logo" width={200} height={75} className="h-auto w-60" />
+    </Link>
 );
-
 export default FitifyLogo;
